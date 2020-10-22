fcdbc7f2d80657298b4a7d77aea681bfe3bebce6
A. Matías Quezada <amatiasq@gmail.com>
2020-10-23 01:38:04 +0200

    Object detection implemented

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,58 @@
-import { context, fillPage, getSize, renderText } from './canvas';
-import { loadImage } from './util/loadImage';
+import { fillPage, getSize, context, renderText } from './canvas';
+import { captureVideo } from './util/captureVideo';
 import { scaleAndCenter } from './util/scaleAndCenter';
 
+declare global {
+  const ml5: any;
+}
+
+fillPage();
+
 (async () => {
-  fillPage();
   printLoading();
+  const video = await captureVideo();
+  const detector = await ml5.objectDetector('cocossd');
+  let detections: any[] = [];
 
-  const image = await loadImage('https://repos.amatiasq.com/genara/img/3.png');
-
+  detect();
   frame();
 
   function frame() {
     const screen = getSize();
-    const rect = scaleAndCenter(image, screen);
-
-    context.clearRect(0, 0, screen.width, screen.height);
-    context.drawImage(image, rect.x, rect.y, rect.width, rect.height);
-
-    renderText(
-      'Hello world',
-      screen.width / 2,
-      screen.height / 2,
-      '40px arial',
-      'white',
-      'black',
+    const rect = scaleAndCenter(
+      {
+        width: video.videoWidth,
+        height: video.videoHeight,
+      },
+      screen,
     );
 
+    video.width = rect.width;
+    video.height = rect.height;
+
+    context.clearRect(0, 0, screen.width, screen.height);
+    context.drawImage(video, rect.x, rect.y, rect.width, rect.height);
+
+    detections.forEach((entity) => {
+      const x = entity.x + rect.x;
+      const y = entity.y + rect.y;
+      const conf = Math.round(entity.confidence * 100);
+
+      context.textAlign = 'left';
+      context.strokeStyle = 'red';
+      context.lineWidth = 3;
+
+      context.strokeRect(x, y, entity.width, entity.height);
+      renderText(entity.label, x + 10, y + 24, '24px Arial');
+      renderText(`${conf}%`, x + 10, y + 50, '16px Arial');
+    });
+
     requestAnimationFrame(frame);
+  }
+
+  async function detect() {
+    detections = await detector.detect(video);
+    detect();
   }
 })();
 
--- src/util/loadImage.ts
+++
@@ -1,8 +0,0 @@
-export function loadImage(src: string) {
-  return new Promise<HTMLImageElement>((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = reject;
-    img.src = src;
-  });
-}
